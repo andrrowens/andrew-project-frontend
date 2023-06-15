8b340c673e19836f7a08b0f9d2838d1ad0d003b9
Andrew Owens <andrrowens@gmail.com>
2023-06-15 17:18:11 -0600

    initial andrew page set up

--- src/components/AndrewCard.js
+++ src/components/AndrewCard.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 
-const AndrewCard = () => {
+const AndrewCard = ( { id, name, background, description }) => {
+
+
+
   return (
-    <div>AndrewCard</div>
+    <div className="andrew-box" id={id}>
+        <div className="andrew-card"> 
+          {/* <h1 className="andrew-name">Andrew Owens</h1> */}
+
+          <strong>Name: </strong>{name} <br/>
+          <strong>Background: </strong>{background} <br/>
+          <strong>Description: </strong>{description} <br/>
+        </div>
+
+    </div>
   )
 }
 
